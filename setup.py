9a2529447ae2e5a25c0e7bba7dd142cb37ea5550
Fabian von Feilitzsch <fabian@fabianism.us>
2015-06-04 10:38:50 -0400

    made setup.py parse requirements from requirements.txt

--- setup.py
+++ setup.py
@@ -1,5 +1,13 @@
-from distutils.core import setup
+from setuptools import setup
+from pip.download import PipSession
+from pip.req import parse_requirements
+
 VERSION = '0.1'
+
+install_reqs = map(
+    lambda ir: str(ir.req),
+    parse_requirements('requirements.txt', session=PipSession())
+)
 
 setup(
     name='sharepa',
@@ -10,4 +18,5 @@ setup(
     author_email='fabian@cos.io',
     url='https://github.com/fabianvf/sharepa',
     download_url='https://github.com/fabianvf/sharepa/tarball/{}'.format(VERSION),
+    install_reqs=install_reqs
 )
