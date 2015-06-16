4f2c3df24a59a7c287e59ec7d9b11922e7c49412
erinspace <erin.braswell@gmail.com>
2015-06-16 16:30:55 -0400

    Add test for no title search

--- tests/test_search.py
+++ tests/test_search.py
@@ -1,3 +1,4 @@
+from sharepa.search import ShareSearch
 from sharepa.search import basic_search
 
 
@@ -5,3 +6,16 @@ def test_basic_search():
     results = basic_search.execute()
     assert results.hits
     assert results.aggregations
+
+
+def test_no_title_search():
+    my_search = ShareSearch()
+
+    my_search = my_search.query(
+        'query_string',
+        query='NOT title:*',
+        analyze_wildcard=True
+    )
+    results = my_search.execute()
+    for result in results:
+        assert not result.get('title')
