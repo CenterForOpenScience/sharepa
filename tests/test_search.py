f71956ff79fe395530eb0b2b97d90c6552f13337
erinspace <erin.braswell@gmail.com>
2015-06-16 17:34:48 -0400

    Add 3 more tests for the search module

--- tests/test_search.py
+++ tests/test_search.py
@@ -1,6 +1,8 @@
 from sharepa.search import ShareSearch
 from sharepa.search import basic_search
 
+import elasticsearch_dsl
+
 
 def test_basic_search():
     results = basic_search.execute()
@@ -19,3 +21,19 @@ def test_no_title_search():
     results = my_search.execute()
     for result in results:
         assert not result.get('title')
+
+
+def test_execute():
+    my_search = ShareSearch()
+
+    result = my_search.execute()
+    assert isinstance(result, elasticsearch_dsl.result.Response)
+
+
+def test_count():
+    count = basic_search.count()
+    assert isinstance(count, int)
+
+
+def test_query():
+    assert isinstance(basic_search._query(basic_search.to_dict()), dict)
