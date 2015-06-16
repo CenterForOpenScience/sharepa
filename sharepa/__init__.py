f9f3083b3f03f8b3a9cba760b51cb50edd7f8827
erinspace <erin.braswell@gmail.com>
2015-06-16 16:30:02 -0400

    move source count to helpers function

--- sharepa/__init__.py
+++ sharepa/__init__.py
@@ -1,12 +1,3 @@
-from sharepa.helpers import source_agg
 from sharepa.search import ShareSearch, basic_search  # noqa
 from sharepa.analysis import bucket_to_dataframe, merge_dataframes  # noqa
-
-
-def source_counts():
-    query = ShareSearch()
-    query.aggs.bucket('sourceAgg', source_agg)
-    return bucket_to_dataframe(
-        'total_source_counts',
-        query.execute().aggregations.sourceAgg.buckets
-    )
+from sharepa.helpers import source_agg, source_counts
--- sharepa/helpers.py
+++ sharepa/helpers.py
@@ -1,3 +1,16 @@
 from elasticsearch_dsl import A
 
+from sharepa.helpers import source_agg
+from sharepa.search import ShareSearch, basic_search  # noqa
+from sharepa.analysis import bucket_to_dataframe, merge_dataframes  # noqa
+
+
 source_agg = A('terms', field='_type', size=0, min_doc_count=0)
+
+def source_counts():
+    query = ShareSearch()
+    query.aggs.bucket('sourceAgg', source_agg)
+    return bucket_to_dataframe(
+        'total_source_counts',
+        query.execute().aggregations.sourceAgg.buckets
+    )
