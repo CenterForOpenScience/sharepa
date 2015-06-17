fd909f383ab8a930c8a858144e0566075821f019
erinspace <erin.braswell@gmail.com>
2015-06-17 10:54:08 -0400

    Add vcr to scan test

--- tests/test_search.py
+++ tests/test_search.py
@@ -1,8 +1,9 @@
 from sharepa.search import ShareSearch
 from sharepa.search import basic_search
 
-import elasticsearch_dsl
+import vcr
 import types
+import elasticsearch_dsl
 
 
 def test_basic_search():
@@ -24,11 +25,16 @@ def test_no_title_search():
         assert not result.get('title')
 
 
+@vcr.use_cassette('tests/vcr/simple_execute.yaml')
 def test_execute():
     my_search = ShareSearch()
-
     result = my_search.execute()
+    first_result = result.hits[0].to_dict()
+
+    assert len(result.hits) == 10
+    assert result.to_dict().keys() == ['hits', '_shards', 'took', 'timed_out', 'time']
     assert isinstance(result, elasticsearch_dsl.result.Response)
+    assert first_result['title'] == 'Avian community structure and incidence of human West Nile infection'
 
 
 def test_count():
@@ -40,15 +46,14 @@ def test_query():
     assert isinstance(basic_search._query(basic_search.to_dict()), dict)
 
 
+@vcr.use_cassette('tests/vcr/scan.yaml')
 def test_scan():
     my_search = ShareSearch()
     my_search = my_search.query(
         'query_string',
-        query='science AND cows AND milk'
+        query='squared AND circle'
     )
-
     scan = my_search.scan()
     scan_list = [item for item in scan]
-
-    assert isinstance(scan, types.GeneratorType)
-    assert scan_list
+    assert len(scan_list) == 3
+    assert scan_list[0].title == '<p>The ellipsoids in the figure are isolines of constant density of bivariate Gaussian distributions.</p>'
