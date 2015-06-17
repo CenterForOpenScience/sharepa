fd6c7386cfdaa5fb97a428b323fc1f9b17f9f02c
erinspace <erin.braswell@gmail.com>
2015-06-17 11:47:57 -0400

    Add pytest fail check on raising pretty print exeption

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,12 +1,21 @@
+import vcr
 import pandas
+import pytest
 
+from sharepa.search import ShareSearch
 from sharepa.helpers import pretty_print
 from sharepa.helpers import source_counts
 
 
+@vcr.use_cassette('tests/vcr/simple_execute.yaml')
 def test_pretty_print():
-    some_stuff = '{"Dusty": "Rhodes"}'
-    pretty_print(some_stuff)
+    my_search = ShareSearch()
+    result = my_search.execute()
+    the_dict = result.to_dict()
+    try:
+        pretty_print(the_dict)
+    except:
+        pytest.fail("Unexpected exception!!")
 
 
 def test_source_counts():
