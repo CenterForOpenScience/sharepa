1130a1623bc58118609ce04cc4983dadc7ee9402
Ben Yetton <bdyetton@gmail.com>
2015-06-22 14:49:03 -0400

    added testing and fixed crossing and 3 level nesting

--- sharepa/experimental_analysis_functions.py
+++ sharepa/experimental_analysis_functions.py
@@ -1,5 +1,5 @@
-
 import pandas as pd
+
 
 def convert_nested_to_dataframe(agg, dates_as_key=True):
     '''A function that takes nested elasticsearch response with aggregation and returns a nested dataframe
@@ -30,43 +30,66 @@ def convert_nested_to_dataframe(agg, dates_as_key=True):
         NY           2002       male        5
         NY           2002       female      5
 
-        What we want to do is step down through all the layers of nested data (recursively) until we reach the end,
+        What we do is step down through all the layers of nested data (recursively) until we reach the end,
         and from the end, start creating pandas dataframes that get merged back into one giant dataframe
 
-        this function is in an experimental state, and currently only tested on 2 nested levels, crossed date does not work
-        :param agg: an aggregation from elasticsearch results
-        :type agg: elasticsearch response.aggregation.agg_name object
-        :returns: pandas data frame of one or two dimetions depending on input data
+        this function is in an experimental state, and currently only tested on 3 nested levels,
+        TODO crossed data does not work
+        :param agg: an aggregation from elasticsearch results with nesting
+        :type agg: elasticsearch response.aggregation object
+        :returns: pandas data frame like example above, with nested data
     '''
+    crossed_cats_expanded = []
+    high_level_returning = False
     agg_as_dict = agg.to_dict()
     cat_names = [item for item in agg_as_dict.keys() if type(agg_as_dict[item]) is dict]
-    for cat_name in cat_names: #FIXME deal with multiple aggergations at the same level
+    for cat_name in cat_names:  # TODO deal with multiple aggregations at the same level (Crossing)
         expanded_buckets = []
         merge_vert = False
+        if not len(getattr(agg, cat_name).buckets):
+            raise ValueError('There is no count data in the lowest level of nesting. Is your search setup correctly?')
+
         for bucket in getattr(agg, cat_name).buckets:
             bucket_as_dict = bucket.to_dict()
             if dict not in [type(item) for item in bucket_as_dict.values()]:
                 # we are at lowest level, begin return
-                if ('key_as_string' in bucket_as_dict.keys()) and dates_as_key: #change dates to readble format
+                if ('key_as_string' in bucket_as_dict.keys()) and dates_as_key:  # change dates to readble format
                     bucket_as_dict['key'] = bucket['key_as_string']
+                    bucket_as_dict.pop('key_as_string')
 
-                bucket_as_dict[cat_name] = bucket_as_dict.pop('key') #change the name of the key to something meaningful
-                expanded_buckets.append(bucket_as_dict) #combine each dict at the lowest level
-
+                bucket_as_dict[cat_name] = bucket_as_dict.pop(
+                    'key')  # change the name of the key to something meaningful
+                expanded_buckets.append(bucket_as_dict)  # combine each dict at the lowest level
             else:
-                #We are at some level other than th lowest
-                level_name = str(bucket.key) #save the name of this level
-                lower_level_return = convert_nested_to_dataframe(bucket) #and drop down into the next level
-                cat_name_dataframe = pd.DataFrame([level_name for i in range(0,lower_level_return.shape[0])]) #create a cat name column
-                cat_name_dataframe.columns = [cat_name] #name the column something meaningful
-                merged_names_dataframe = pd.concat([cat_name_dataframe, lower_level_return], axis=1) #add return dataframes from lower levels, and attach the cat name coloum
-                expanded_buckets.append(merged_names_dataframe) #combine each cat and its data
+                # We are at some level other than the lowest
+                level_name = str(bucket.key)  # save the name of this level
+                lower_level_return = convert_nested_to_dataframe(bucket)  # and drop down into the next level
+                expanded_buckets.append(add_category_labels(level_name,cat_name,lower_level_return))
                 merge_vert = True
-        # if merge_vert:
-        #     dataframe_out = pd.concat(expanded_buckets, axis=0)
-        #     return dataframe_out
         if not merge_vert:
             dataframe_out = pd.DataFrame(expanded_buckets)
             dataframe_out.rename(columns=lambda x: x.replace('key', cat_name))
-            return dataframe_out # FIXME this return here means we cannot add in other catogories at the same level (cant deal with corssing)
-    return pd.concat(expanded_buckets, axis=0)
+            crossed_cats_expanded.append(dataframe_out.reset_index(drop=True))
+            high_level_returning = True
+
+    if high_level_returning:
+        return pd.concat(crossed_cats_expanded, axis=1).reset_index(drop=True)
+    else:
+        return pd.concat(expanded_buckets, axis=0).reset_index(drop=True)
+
+
+def add_category_labels(level_name,cat_name,dataframe_needing_cat):
+    '''A function that adds a category name column to a pandas dataframe
+
+        :param level_name: an aggregation from elasticsearch results with nesting
+        :type level_name: elasticsearch response.aggregation object
+        :param cat_name: an aggregation from elasticsearch results with nesting
+        :type cat_name: elasticsearch response.aggregation object
+        :param dataframe_needing_cat: a pandas dataframe to append category name too
+        :type dataframe_needing_cat: elasticsearch response.aggregation object
+        :returns: pandas data frame like example above, with nested data
+    '''
+    cat_name_dataframe = pd.DataFrame(
+        [level_name for i in range(0, dataframe_needing_cat.shape[0])])  # create a cat name column
+    cat_name_dataframe.columns = [cat_name]  # name the column something meaningful
+    return pd.concat([cat_name_dataframe, dataframe_needing_cat], axis=1)
